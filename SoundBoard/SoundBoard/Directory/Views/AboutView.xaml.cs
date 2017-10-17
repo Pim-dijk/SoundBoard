b99db27daeafa7530e11349df4b92d0acab453a1
Pim-dijk <pim.dijk@student.graafschapcollege.nl>
2017-10-17 14:26:47 +0200

    Renamed folder structure

---
+++ SoundBoard/SoundBoard/Directory/Views/AboutView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SoundBoard.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class AboutView : Window
+    {
+        public AboutView(object dataContext)
+        {
+            this.DataContext = DataContext;
+            InitializeComponent();
+        }
+    }
+}
---
+++ SoundBoard/SoundBoard/Directory/Views/ChangeNameView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SoundBoard.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangeNameView.xaml
+    /// </summary>
+    public partial class ChangeNameView : Window
+    {
+        public ChangeNameView(object dataContext)
+        {
+            this.DataContext = dataContext;
+            InitializeComponent();
+        }
+    }
+}
